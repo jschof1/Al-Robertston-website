2429ccf17e163eaf07ed7f22f44a5d53c1aea667
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2020-12-12 16:53:54 +0000

    loading image

--- showcase/loading.js
+++ showcase/loading.js
@@ -1,17 +1,1 @@
-function init() {
-    var imgDefer = document.querySelectorAll(".img-wrap");
-    var lastLoadIndex = 0;
-    function loadNextImage() {
-      if (imgDefer.length === lastLoadIndex) {
-        return;
-      }
-      imgDefer[lastLoadIndex].addEventListener("load", loadNextImage);
-      imgDefer[lastLoadIndex].setAttribute(
-        "src",
-        imgDefer[lastLoadIndex].getAttribute("data-src")
-      );
-      lastLoadIndex += 1;
-    };
-    loadNextImage();
-  }
-  window.onload = init;
+window.setTimeout("document.getElementById('loading').style.display='none';", 5000)
